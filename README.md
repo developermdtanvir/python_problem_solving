ba11737a9b436c71f1ac48bd3a9810ee198e9811
devmdtanvir <officialtanvir@hotmail.com>
2023-03-06 15:21:40 +0600

    array number check for unique number to new array

--- README.md
+++ README.md
@@ -9,3 +9,5 @@
 - container only the strings that have a length gether then 5
 - contains only the even integers from the original list
 - vowel number count from string
+- find largest number an array
+- array contains check unique number an array
--- app.py
+++ app.py
@@ -9,3 +9,15 @@ def find_longest_string(strings):
 
 
 print("find largest string an array:",find_longest_string(["Tanvir","Shakib","jamal","Tanvir Hossain","kamal uddin"]))
+
+# array contains check unique number an array 
+
+def unique_number_array(numbers):
+    unique_number = []
+    for number  in numbers : 
+        if number not in unique_number: 
+            unique_number.append(number)
+    return unique_number
+
+
+print(unique_number_array([10,20,30,40,20,40,40,50,60]))
